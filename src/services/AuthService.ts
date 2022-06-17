a704b7a9a7238d2cdcc7f2caab161b72a9777a99
BuildTools <sqartox@gmail.com>
2022-06-17 10:53:27 +0200

    Notify on user connection attempt

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,13 +1,19 @@
 import mysql from '../mysql'
 import { createHash } from 'node:crypto'
 import { RowDataPacket } from 'mysql2'
+import client from '../mqtt'
 
 export default {
   login: async function ({ email, password }: {email:string, password:string}) {
     const hash = createHash('sha256')
     hash.update(password)
     const user = (await mysql.query<RowDataPacket[]>('SELECT * FROM users WHERE email=? AND password=?', [email, hash.digest().toString()]))[0][0]
-    if (user) delete user.password
+    if (user) {
+      delete user.password
+      await client.publish('auth/connection/success', JSON.stringify(user))
+    } else {
+      await client.publish('auth/connection/fail', JSON.stringify([email]))
+    }
     return user
   }
 }
